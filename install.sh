bd906c8632b83c0d6223d9c7f183cddeb82e7f15
SkyDev <67011352+SkyDev06@users.noreply.github.com>
2025-09-21 19:15:03 +0700

    Add files via upload

--- install.sh
+++ install.sh
@@ -5,14 +5,14 @@ ENDCOLOR="\e[0m"
 clear
 echo -e "${GREEN}Installing Proxy...${ENDCOLOR}"
 sleep 1
-if [ -f "AnchProxy" ]; then
+if [ -f "LuckyProxy_Beta" ]; then
     echo -e "${RED}Deleting old proxy...${ENDCOLOR}"
-    rm AnchProxy
+    rm LuckyProxy_Beta
     sleep 1
     echo -e "${GREEN}Getting proxy...${ENDCOLOR}"
 fi
-wget -q https://github.com/AyamNubCh/AnchProxy/raw/main/AnchProxy
+wget -q https://github.com/AyamNubCh/AnchProxy/raw/main/LuckyProxy_Beta
 sleep 1
 echo -e "${GREEN}Proxy Installed${ENDCOLOR}"
-echo -e "${GREEN}Execute proxy with this command: ./AnchProxy${ENDCOLOR}"
-chmod +x AnchProxy
+echo -e "${GREEN}Execute proxy with this command: ./LuckyProxy_Beta${ENDCOLOR}"
+chmod +x LuckyProxy_Beta
